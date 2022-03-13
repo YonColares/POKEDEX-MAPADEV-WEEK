4fcf37ad0eb8bcff7697242545ae1a11bcb57f44
Yon Gabriel Colares Pinto <98499223+YonColares@users.noreply.github.com>
2022-03-13 11:50:23 -0400

    commit inicial

---
+++ README.md
@@ -0,0 +1,6 @@
+Ola!
+
+Neste projeto, mentoriado pela DEV EM DOBRO melhorei meus conhecimentos na tríade HTMLL, CSS e JS.
+Entender como funciona cada um está se tornando cada vez mais claro.
+
+Rumo ao próximo aprendizado!
---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+//Sempre fazer o passo a passo antes como comentáriopara não se perder na lógica dos códigos. Sempre pensando em como vai funcionar e na ordem que vai se seguir.
+
+const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelectorAll('.cartao-pokemon')
+
+listaSelecaoPokemons.forEach(pokemon => {
+    
+    pokemon.addEventListener('click', () => {
+        const cartaoPokemonAberto = document.querySelector('.aberto')        
+        cartaoPokemonAberto.classList.remove('aberto')
+
+        const idPokemonSelecionado = pokemon.attributes.id.value
+
+        const idDoCartaoPokemonParaAbrir = 'cartao-' + idPokemonSelecionado
+        const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoPokemonParaAbrir)
+        cartaoPokemonParaAbrir.classList.add('aberto')
+
+        const pokemonAtivoNaListagem = document.querySelector('.ativo')
+        pokemonAtivoNaListagem.classList.remove('ativo')
+
+        const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
+        pokemonSelecionadoNaListagem.classList.add('ativo')
+
+    })
+})
